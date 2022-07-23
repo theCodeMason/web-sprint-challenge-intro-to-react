08dedd082504ca61f33e83541980424d469578f0
Ubuntu <ubuntu@ip-172-31-41-148.us-west-2.compute.internal>
2022-07-23 03:30:22 +0000

    Adding super mvp answers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Character from "./components/Character";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  function fetchData() {
+    fetch("https://swapi.dev/api/people/")
+      .then((resStream) => {
+        return resStream.json();
+      })
+      .then((characterFromApi) => {
+        console.log(characterFromApi);
+        // update the array above
+        setCharacters(characterFromApi);
+      });
+  }
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+ return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* {characters.map((character)=>{
+        return <div>{character}</div>
+      })} */}
+      {characters.map((character) => {
+        return <Character key={character.name} name={character.name} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+font-size: large;
+color: #2e557c;
+`
+
+const Character = (props) => {
+    return <StyledDiv>{props.name}</StyledDiv>
+}
+
+export default Character
